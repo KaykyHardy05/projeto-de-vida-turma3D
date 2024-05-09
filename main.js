00b4b8f21b539aac040ea918586e6d7a573d811b
KaykyHardy05 <107191827+KaykyHardy05@users.noreply.github.com>
2024-05-08 21:07:07 -0300

    bah3

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
+const abastextos = document.querySelectorAll(".abas-conteudo")
 
 //Percorrer (iterar) uma lista de elementos
 for(let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j = 0; j < botoes.length; j++){
-               botoes[j].classList.remove("ativo")            
+               botoes[j].classList.remove("ativo")
+               abastextos[j].classList.remove("ativo")            
         }
         botoes[i].classList.add("ativo")
+        abastextos[i].classList.add("ativo")
     }
 }
